449816813a311193643d7b617306ea50cf49c36b
parambedi1453 <parambedi1453@gmail.com>
2019-10-18 02:43:06 +0530

    suffix array

--- suffixArray/Anagram.java
+++ suffixArray/Anagram.java
@@ -1,10 +1,26 @@
 import java.util.*;
 public class Anagram
 {
+    public static String  sortString(String str)
+    {
+        char a[]  = str.toCharArray();
+        Arrays.sort(a);
+
+        return  (new String(a));
+    }
     public static void main(String args[])
     {
-        String arr[] = {"abb","baa","bat","tab","cab","crazy"};
+        String arr[] = {"abb","bba","bat","tab","cab","crazy"};
 
+       
+
+        //sorting individual string
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i] = sortString(arr[i]);
+        } 
+
+         // Sorted array of strings
         String temp;
         for(int i=0;i<arr.length;i++)
         {
@@ -18,9 +34,18 @@ public class Anagram
                 }
             }
         }
-        for(int i=0;i<arr.length;i++)
+
+        int c =0;
+        for(int i=0;i<arr.length-1;i++)
         {
             System.out.println(arr[i]);
+            if(arr[i].equals(arr[i+1]))
+            c++;
         }
+
+        System.out.println(c);
+       
+       
+
     }
 }
