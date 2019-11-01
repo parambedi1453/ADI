03e3a91f4f9ccf33600ce2afa10bafbdd4fde209
parambedi1453 <parambedi1453@gmail.com>
2019-11-02 05:28:54 +0530

    dp

--- dp/Lcs.cpp
+++ dp/Lcs.cpp
@@ -1,5 +1,31 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
+void printLcs(int k[100][100],int i,int j,string x,string y,string str)
+{
+    if(i==0||j==0)
+    {
+       string k = str;
+       reverse(k.begin(),k.end());
+       cout<<k<<endl;
+       return ;
+    }
+    // if same then add char to string and call for previous diagnal
+    if(x[i-1]==y[j-1])
+    {
+        str=str+x[i-1];
+        printLcs(k,i-1,j-1,x,y,str);
+    }
+    // if not then check from where the max came from if came and accordingly call for that matrix index
+    else
+    {
+        if(k[i-1][j]>k[i][j-1])
+        printLcs(k,i-1,j,x,y,str);
+        else
+        printLcs(k,i,j-1,x,y,str);
+    }
+    
+}
 int main()
 {
     string x, y;
@@ -8,7 +34,7 @@ int main()
     cin>>y;
     int row = x.size()+1;
     int col = y.size()+1;
-    int k[row][col];
+    int k[100][100];
 
     for(int i=0;i<row;i++)
     k[0][i]=0;
@@ -29,5 +55,10 @@ int main()
     }
     cout<<k[row-1][col-1]<<endl;
     
+    string ans="";
+
+
+    // printing of the longest commom susequence
+    printLcs(k,row-1,col-1,x,y,ans);
 
 }
