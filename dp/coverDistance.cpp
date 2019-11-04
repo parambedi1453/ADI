d0e4c582b9dff0a6f39972a76aaaeb89424fadbf
parambedi1453 <parambedi1453@gmail.com>
2019-11-05 04:23:32 +0530

    dp

--- dp/coverDistance.cpp
+++ dp/coverDistance.cpp
@@ -1,3 +1,27 @@
+/*
+    A frog jumps either 1, 2 or 3 steps to go to top. In how many ways can it reach the top.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. T testcases follow. Each testcase contains one line of input N denoting the total number of steps.
+
+Output:
+For each testcase, in a new line, print the number of ways to reach the top.
+
+Constraints:
+1 ≤ T ≤ 100
+1 ≤ N ≤ 50
+
+Example:
+Input:
+2
+1
+5
+Output:
+1
+13
+
+*/
+
 #include <iostream>
 using namespace std;
 int getWays(int n)
