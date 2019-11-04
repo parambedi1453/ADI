8f75e4e86fdb4249db8716bcd017a8719d9a0dec
parambedi1453 <parambedi1453@gmail.com>
2019-11-05 03:55:09 +0530

    dp

--- dp/editDistance.cpp
+++ dp/editDistance.cpp
@@ -1,3 +1,35 @@
+/*
+    Given two strings str1 and str2 and below operations that can performed on str1. Find minimum number of edits (operations) required to convert ‘str1′ into ‘str2′.
+
+Insert
+Remove
+Replace
+All of the above operations are of cost=1.
+Both the strings are of lowercase.
+
+Input:
+The First line of the input contains an integer T denoting the number of test cases. Then T test cases follow. Each tese case consists of two lines. The first line of each test case consists of two space separated integers P and Q denoting the length of the strings str1 and str2 respectively. The second line of each test case coantains two space separated strings str1 and str2 in order.
+
+Output:
+Corresponding to each test case, pirnt in a new line, the minimum number of operations required.
+
+Constraints:
+1 <= T <= 50
+1 <= P <= 100
+1 <= Q <= 100
+
+Example:
+Input:
+1
+4 5
+geek gesek
+
+Output:
+1
+
+Explanation:
+Testcase 1: One operation is required to make 2 strings same i.e. removing 's' from str2.
+*/
 #include <iostream>
 #include<algorithm>
 #include<string.h>
@@ -18,12 +50,13 @@ int getMinCost(string s1,string s2)
     {
         for(int j=1;j<col;j++)
         {
-            if(s2[i-1]==s1[j-1])
+            if(s2[i-1]==s1[j-1])//same then no operation to be performed hence get previous result
             {
                 k[i][j] = k[i-1][j-1];
             }
             else
             {
+                // if not same then the evaluating all the threee opationa snd getting which operation would give the min
                 int replace  = 1+k[i-1][j-1];
                 int del = 1+k[i-1][j];
                 int insert = 1+k[i][j-1];
