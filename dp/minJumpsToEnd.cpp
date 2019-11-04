5be4951f4f9f1568234444de96d28b11e3bc20e4
parambedi1453 <parambedi1453@gmail.com>
2019-11-05 03:13:32 +0530

    dp

--- dp/minJumpsToEnd.cpp
+++ dp/minJumpsToEnd.cpp
@@ -1,3 +1,35 @@
+/*
+
+        Given an array of integers where each element represents the max number of steps that can be made forward from that element. The task is to find the minimum number of jumps to reach the end of the array (starting from the first element). If an element is 0, then cannot move through that element.
+
+Input: 
+The first line contains an integer T, depicting total number of test cases.  Then following T lines contains a number n denoting the size of the array. Next line contains the sequence of integers a1, a2, ..., an.
+
+Output:
+For each testcase, in a new line, print the minimum number of jumps. If answer is not possible print "-1"(without quotes).
+
+Constraints:
+1 ≤ T ≤ 100
+1 ≤ N ≤ 107
+0 <= ai <= 107
+
+Example:
+Input:
+2
+11
+1 3 5 8 9 2 6 7 6 8 9
+6
+1 4 3 2 6 7
+Output:
+3
+2
+
+Explanation:
+Testcase 1: First jump from 1st element, and we jump to 2nd element with value 3. Now, from here we jump to 5h element with value 9. and from here we will jump to last.
+
+*/
+
+
 #include <iostream>
 #include<climits>
 using namespace std;
@@ -54,3 +86,6 @@ int main() {
 	}
 	return 0;
 }
+
+// int this b[i] indicates the minimum number of jumps needed to reach a[n-1] from arr[i]. Finally, we return arr[0].
+// if 0 then we cannnot reach to end from there hence INT_MAX for not reaching there
