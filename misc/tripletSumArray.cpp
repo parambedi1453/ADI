0fbfe4cfcc287572d607d269906316ab11e5fe1d
parambedi1453 <parambedi1453@gmail.com>
2019-11-16 12:09:19 +0530

    misc

--- misc/tripletSumArray.cpp
+++ misc/tripletSumArray.cpp
@@ -1,3 +1,36 @@
+/*
+
+    Given an array A[] of N numbers and another number x, determine whether or not there exist three elements in A[] whose sum is exactly x.
+
+Input:
+First line of input contains number of testcases T. For each testcase, first line of input contains n and x. Next line contains array elements.
+
+Output:
+Print 1 if there exist three elements in A whose sum is exactly x, else 0.
+
+Constraints:
+1 ≤ T ≤ 100
+1 ≤ N ≤ 103
+1 ≤ A[i] ≤ 105
+
+Example:
+Input:
+2
+6 13
+1 4 45 6 10 8
+5 10
+1 2 4 3 6
+
+Output:
+1
+1
+
+Explanation:
+Testcase 1: There is one triplet with sum 13 in the array. Triplet elements are 1, 4, 8, whose sum is 13.
+ 
+
+*/
+
 #include <iostream>
 #include<algorithm>
 using namespace std;
